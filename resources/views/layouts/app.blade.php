acedac395c1f1af94b4efacef271f7ee5901b552
febeFlo <steffanyflorence@gmail.com>
2024-05-04 15:14:26 +0700

    Customer History

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,7 +37,7 @@
                                 aria-current="page">Home</a>
                         </li>
                         <li>
-                            <a href="cart"
+                            <a href="custCart"
                                 class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-yellow-500 md:p-0 dark:text-white md:dark:hover:text-yellow-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Cart</a>
                         </li>
                         <li>
@@ -45,7 +45,7 @@
                                 class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-yellow-500 md:p-0 dark:text-white md:dark:hover:text-yellow-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Chats</a>
                         </li>
                         <li>
-                            <a href="#"
+                            <a href="custHistory"
                                 class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-yellow-500 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">History</a>
                         </li>
                         <li>
--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,23 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('Customer.index');
+});
+
+Route::get('jastiperInfo', function () {
+    return view('Customer.jastipper_info');
+});
+
+Route::get('custCart', function () {
     return view('Customer.cart');
 });
 
 Route::get('custChats', function () {
     return view('Customer.chats');
+});
+
+Route::get('custHistory', function () {
+    return view('Customer.history');
 });
 
 Route::get('custAccount', function () {
