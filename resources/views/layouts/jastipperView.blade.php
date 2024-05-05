313ed23a42b212e1b92841a8762666621ed2e10a
febeFlo <steffanyflorence@gmail.com>
2024-05-05 15:41:02 +0700

    If  jastiper logout -> Home

--- resources/views/layouts/jastipperView.blade.php
+++ resources/views/layouts/jastipperView.blade.php
@@ -55,7 +55,7 @@
                                 class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-yellow-500 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Account</a>
                         </li>
                         <li>
-                            <a href="#"
+                            <a href="/"
                                 class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-yellow-500 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Log Out</a>
                         </li>
                     </ul>
