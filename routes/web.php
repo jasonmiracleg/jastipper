4957f20dbfd95a4537a68bc6ff1e42ffa6c9f378
febeFlo <steffanyflorence@gmail.com>
2024-05-09 14:12:27 +0700

    add route

--- routes/web.php
+++ routes/web.php
@@ -45,3 +45,7 @@ Route::get('jastiperHistory', function () {
 Route::get('jastiperAccount', function () {
     return view('Jastiper.account');
 });
+
+Route::get('jastiperRoute', function() {
+    return view('Jastiper.route');
+});
