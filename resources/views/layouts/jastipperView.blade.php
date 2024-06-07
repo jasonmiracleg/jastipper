78691d920f5b471ebbb52ff4e78183abaf932b83
jasonmiracleg <jasonmiracle15@gmail.com>
2024-06-07 08:12:52 +0700

    Add Logo to Navbar

--- resources/views/layouts/jastipperView.blade.php
+++ resources/views/layouts/jastipperView.blade.php
@@ -17,6 +17,7 @@
         <nav class="p-[10px] bg-yellow-100 border-gray-200">
             <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+                    <img src="{{ asset('assets/logo_jastipper.png') }}" class="h-8" alt="Flowbite Logo" />
                     <span class="self-center text-2xl font-semibold whitespace-nowrap">Jastip Miracle</span>
                 </a>
                 <button data-collapse-toggle="navbar-default" type="button"
